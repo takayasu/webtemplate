6a157b20aa2f1afbab0217511c9f15170a3b24af
Atsushi Takayasu <atsushi.takayasu@bigtreetc.com>
2015-11-27 18:36:08 +0900

    基本的な動作について展開
    Signed-off-by: Atsushi Takayasu <atsushi.takayasu@bigtreetc.com>

--- src/main/java/com/yurugee/tech/web/controller/TestController.java
+++ src/main/java/com/yurugee/tech/web/controller/TestController.java
@@ -1,8 +1,16 @@
 package com.yurugee.tech.web.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.yurugee.tech.web.BeanConfig;
 import com.yurugee.tech.web.dao.EmployeeDao;
@@ -24,7 +32,6 @@ public class TestController {
 	@RequestMapping("index")
 	public String index(){
 
-		System.out.println(config.dataSource().toString());
 
 		Employee emp = empDao.selectById("1");
 
@@ -32,5 +39,62 @@ public class TestController {
 		return "test-index";
 
 	}
+
+	@RequestMapping("list")
+	public ModelAndView list(){
+
+
+
+		List<Employee> list = empDao.allList();
+
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("list", list);
+		mv.setViewName("test-list");
+
+		return mv;
+
+	}
+
+	@RequestMapping(path="add")
+	public ModelAndView add(@RequestParam Map<String, String> params) throws UnsupportedEncodingException{
+
+		Employee emp = new Employee();
+		emp.id = params.get("id");
+		emp.name = params.get("name");
+
+		empDao.insert(emp);
+
+
+		List<Employee> list = empDao.allList();
+
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("list", list);
+		mv.setViewName("test-list");
+
+		return mv;
+
+	}
+
+
+	@RequestMapping(path="add2" ,produces="text/plain;charset=UTF-8")
+	public ModelAndView requestAdd(HttpServletRequest request) throws UnsupportedEncodingException{
+
+		Employee emp = new Employee();
+		emp.id = request.getParameter("id");
+		emp.name = request.getParameter("name");
+
+		empDao.insert(emp);
+
+
+		List<Employee> list = empDao.allList();
+
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("list", list);
+		mv.setViewName("test-list");
+
+		return mv;
+
+	}
+
 
 }
--- src/main/java/com/yurugee/tech/web/model/Employee.java
+++ src/main/java/com/yurugee/tech/web/model/Employee.java
@@ -1,8 +1,10 @@
 package com.yurugee.tech.web.model;
 
 import org.seasar.doma.Entity;
+import org.seasar.doma.Table;
 
 @Entity
+@Table(name="emp")
 public class Employee {
 
 	public String id;
