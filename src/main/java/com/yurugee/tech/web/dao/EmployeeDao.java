8bdff1ddeb12f511e7c64ea3099fdd1280314e49
Atsushi Takayasu <atsushi.takayasu@bigtreetc.com>
2015-12-01 14:44:20 +0900

    fixed #3
    Signed-off-by: Atsushi Takayasu <atsushi.takayasu@bigtreetc.com>

--- src/main/java/com/yurugee/tech/web/dao/EmployeeDao.java
+++ src/main/java/com/yurugee/tech/web/dao/EmployeeDao.java
@@ -2,22 +2,15 @@ package com.yurugee.tech.web.dao;
 
 import java.util.List;
 
-import org.seasar.doma.AnnotateWith;
-import org.seasar.doma.Annotation;
-import org.seasar.doma.AnnotationTarget;
 import org.seasar.doma.Dao;
 import org.seasar.doma.Insert;
 import org.seasar.doma.Select;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import com.yurugee.tech.web.model.Employee;
 
 
 @Dao
-@AnnotateWith(annotations = {
-		@Annotation(target = AnnotationTarget.CLASS, type = Component.class),
-		@Annotation(target = AnnotationTarget.CONSTRUCTOR, type = Autowired.class) })
+@DomaRepository
 public interface EmployeeDao {
 
 	@Select
